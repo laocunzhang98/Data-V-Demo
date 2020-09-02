3a40288544dead1022fab0e614c8a38598a2a7a2
laocunzhang98 <782984630@qq.com>
2020-09-02 17:28:38 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+  xxx
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss">
+.home {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background-color: #eee;
+  box-sizing: border-box;
+}
+</style>
